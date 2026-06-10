9569c6a61b5eaf79b5dbafcea3e4efe78498dbf0
artists-colony-inn <aci.safed@gmail.com>
2026-06-10 10:22:37 +0300

    Add files via upload

--- website/components/footer.js
+++ website/components/footer.js
@@ -19,7 +19,7 @@ document.currentScript.insertAdjacentHTML('afterend', `
       <ul>
 		<li><a href="tel:+972586200779">+972-58-620-0779</a></li>
 		<li style="margin-top:0.75rem;"><a href="https://wa.me/972586200779" target="_blank" rel="noopener" class="btn btn-gold">WhatsApp Us</a></li>
-		<li style="margin-top:0.75rem;">Simtat Yud Zayin 9<br>Safed, Israel 1323100</li>		<li><a href="https://wa.me/972586200779" target="_blank" rel="noopener">WhatsApp</a></li>
+		<li style="margin-top:0.75rem;">Simtat Yud Zayin 9<br>Safed, Israel 1323100</li>
       </ul>
     </div>
   </div>
